30b7d799d6b38eec3543bb47129eb2f2a7b0cd06
tetianaklius <tetyanaklyus@gmail.com>
2023-04-05 18:22:14 +0300

    fix

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import logout_view, login_view, registration_view, profile_view, message_view
+from .views import logout_view, login_view, registration_view, profile_view
 
 app_name = "account"
 
@@ -8,6 +8,5 @@ urlpatterns = [
     path("login/", login_view, name="login_view"),
     path("registration/", registration_view, name="registration_view"),
     path("profile/", profile_view, name="profile"),
-    path("message/", message_view, name="message_view"),
 
 ]
